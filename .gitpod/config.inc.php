63a59c50c893c1eb4a0096aa3340343195ca9d42
Moritz Eysholdt <moritz.eysholdt@typefox.io>
2019-05-22 17:00:49 +0200

    Work on nginx and mysql

---
+++ .gitpod/config.inc.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+$i = 0;
+$i++;
+
+$cfg['Servers'][$i]['auth_type'] = 'config';
+$cfg['Servers'][$i]['user'] = 'root';
+$cfg['Servers'][$i]['password'] = '';
+$cfg['Servers'][$i]['host'] = '127.0.0.1';
+$cfg['Servers'][$i]['compress'] = false;
+$cfg['Servers'][$i]['AllowNoPassword'] = true;
+
+$cfg['UploadDir'] = '';
+$cfg['SaveDir'] = '';
+
+?>
